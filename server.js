2bd484813287c3594ce70cf93eea8460ed85cf9f
Brayden <brayburl224@gmail.com>
2022-10-30 14:43:14 -0400

    added connections to routes as well as const fs and path

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const express = require('express');
 
-const app = express();
+const path = require('path');
+
+const fs = require('fs');
 
 const PORT = process.env.PORT || 3001;
+
+const app = express();
 
 const apiRoutes = require('./routes/apiRoutes');
 
 const htmlRoute = require('./routes/htmlRoute');
 
 app.use(express.static('public'));
+
+app.use(express.urlencoded({extend: true}));
+
+app.use(express.json());
+
+app.use('/api', apiRoutes);
+
+app.use('/', htmlRoute);
 
 app.listen(PORT, () => {
     console.log(`Server is available at localhost${PORT}`);
