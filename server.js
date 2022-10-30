404d223c1b25a5a7246ff644cea064c8e952e7d0
Brayden <brayburl224@gmail.com>
2022-10-30 14:14:10 -0400

    worked a little more on server.js and added route folders

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+const apiRoutes = require('./routes/apiRoutes');
+
+const htmlRoute = require('./routes/htmlRoute');
+
 app.use(express.static('public'));
 
 app.listen(PORT, () => {
