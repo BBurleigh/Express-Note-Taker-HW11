4e882ddfece3179a2700ab1d4a2dace31435c958
Brayden <brayburl224@gmail.com>
2022-11-01 20:17:08 -0400

    continued working on noteRoute.js and created an additional functions.js file

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -2,5 +2,21 @@ const router = require('express').Router();
 
 const {notes} = require('../..db/db.json');
 
+const {noteCreateNewNote, noteDeleteNote} = require('../../assets/js/functions');
+
+router.get('notes', (req, res) => {
+    var saved = notes;
+    res.json(saved);
+})
+
+router.post('notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    res.json(note);
+})
+
+router.delete('/notes/:id', (req, res => {
+    noteDeleteNote(notes, req.param.id);
+    res.json(notes);
+}))
 
 module.exports = router;
