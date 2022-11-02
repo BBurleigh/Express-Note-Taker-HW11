07992afdd2e7db87f7f876a29c259ea9385bbef0
Brayden <brayburl224@gmail.com>
2022-11-02 13:46:38 -0400

    received error message after adding slashes to notes in noteRoutes.js

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -4,12 +4,12 @@ const {notes} = require('../../db/db.json');
 
 const {noteCreateNewNote, noteDeleteNote} = require('../../public/assets/js/functions');
 
-router.get('notes', (req, res) => {
+router.get('/notes', (req, res) => {
     var saved = notes;
     res.json(saved);
 })
 
-router.post('notes', (req, res) => {
+router.post('/notes', (req, res) => {
     req.body.id = notes.length.toString();
     var note = noteCreateNewNote(req.body, notes);
     res.json(note);
