a496c81e71b8f8d6b29eeb684f6d0c122bfbf8f4
Brayden <brayburl224@gmail.com>
2022-11-01 20:27:29 -0400

    began creating function to save a note

---
+++ public/assets/js/functions.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+const fs = require('fs');
+
+function noteCreateNewNote(body, noteTakerArray) {
+    const note = body;
+    noteTakerArray.push(note);
+    fs.writeFileSync(path.join(__dirname, '../../db/db.json')), JSON.stringify({notes: noteTakerArray}, null, 2)
+    return note;
+}
+
+module.exports = {noteCreateNewNote, noteDeleteNote};
